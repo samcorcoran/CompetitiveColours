8029de16069770a1d97518c78442339ff957dcba
willwybrow <will@willwybrow.com>
2014-04-18 15:15:00 +0100

    empty button click handler

--- CompetitiveColours/app/src/main/java/uk/wycor/competitivecolours/app/MainActivity.java
+++ CompetitiveColours/app/src/main/java/uk/wycor/competitivecolours/app/MainActivity.java
@@ -2,9 +2,11 @@ package uk.wycor.competitivecolours.app;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.bluetooth.*;
+import android.view.View;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,6 +14,13 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button_make_discoverable = (Button) findViewById(R.id.button_make_discoverable);
+        button_make_discoverable.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //make a thing happen
+            }
+        });
     }
 
 
