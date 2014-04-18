47353d487ac8a4a51cd1d3bc4a13dee080e387c0
willwybrow <will@willwybrow.com>
2014-04-18 23:53:09 +0100

    message handlers abound
    <no description available>

--- CompetitiveColours/app/src/main/java/uk/wycor/competitivecolours/app/ServerThread.java
+++ CompetitiveColours/app/src/main/java/uk/wycor/competitivecolours/app/ServerThread.java
@@ -1,9 +1,14 @@
 package uk.wycor.competitivecolours.app;
 
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * Created by WillW on 18/04/14.
@@ -11,14 +16,23 @@ import java.io.IOException;
 public class ServerThread extends Thread {
 
     private final BluetoothServerSocket serverSocket;
+    private final Handler handler;
 
-    public ServerThread(BluetoothServerSocket bss) {
+    private HashMap<BluetoothDevice, ConnectedThread> serverThreads;
+
+    public ServerThread(BluetoothServerSocket bss, Handler h) {
+        handler = h;
         serverSocket = bss;
+        serverThreads = new HashMap<BluetoothDevice, ConnectedThread>();
     }
 
     public void run() {
         BluetoothSocket socket = null;
         // Keep listening until exception occurs or a socket is returned
+        Message m = handler.obtainMessage();
+        Bundle b = m.getData();
+        b.putString("key", "Background Task notify, server thread has started");
+        handler.sendMessage(m);
         while (true) {
             try {
                 socket = serverSocket.accept();
@@ -28,9 +42,7 @@ public class ServerThread extends Thread {
             // If a connection was accepted
             if (socket != null) {
                 // Do work to manage the connection (in a separate thread)
-                forkCommunicationThread(socket);
-                cancel();
-                break;
+                forkCommunicationThread(socket, handler);
             }
         }
     }
@@ -43,8 +55,14 @@ public class ServerThread extends Thread {
         }
     }
 
-    private void forkCommunicationThread(BluetoothSocket bts) {
-        ConnectedThread connectedThread = new ConnectedThread(bts);
-        connectedThread.start();
+    private void forkCommunicationThread(BluetoothSocket bts, Handler h) {
+        serverThreads.put(bts.getRemoteDevice(), new ConnectedThread(bts, h));
+        serverThreads.get(bts.getRemoteDevice()).start();
+    }
+
+    public void writeToAllClients(String message) {
+        for (ConnectedThread thread : serverThreads.values()) {
+            thread.write(message.getBytes());
+        }
     }
 }
